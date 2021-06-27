2a79b099e5b175a40060063e0472b32d4d918cb1
sarbanhaxb <sarban7@mail.ru>
2021-06-27 15:00:05 +0300

    14:59 27.06.2021

--- main.py
+++ main.py
@@ -35,17 +35,90 @@ class resourses_sheet(QMainWindow):
         df = pd.DataFrame(ws, columns=['code', 'name', 'unit', 'amount'])
 
         electrodes = 0
-        """расчет электродов"""
+        paints = 0
+        geosynthetic_nt = 0
+        geogrids_nt = 0
+        thermal = 0
+        bituminous = 0
+
         for index, row in df.iterrows():
+            """расчет электродов"""
             if str(row['name']).lower().find('электрод') == 0:
                 if str(row['unit']).lower() == 'кг':
                     electrodes += float(row['amount']) / 1000
                 elif str(row['unit']).lower() == 'т':
                     electrodes += float(row['amount'])
+            """расчет геополотна нетканого"""
+            if (str(row['code']).lower().find('фссц-01.7.12.05') == 0 or str(row['code']).lower().find('01.7.12.05') == 0) and (str(row['name']).lower().find('нетканый') == 0 or str(row['name']).lower().find('геополотно') == 0 or str(row['name']).lower().find('геотекстиль') == 0 or str(row['name']).lower().find('дренажный композит') == 0 or str(row['name']).lower().find('материал геотекстильный нетканый') == 0 or str(row['name']).lower().find('полотно иглопробивное') == 0 or str(row['name']).lower().find('полотно нетканное ') == 0):
+                if str(row['unit']).lower() == 'м2':
+                    geosynthetic_nt += float(row['amount'])
+            """расчет георешетки нетканой """
+            if (str(row['code']).lower().find('фссц-01.7.12.07') == 0 or str(row['code']).lower().find('01.7.12.07') == 0) and str(row['name']).lower().find('георешетка ') == 0:
+                if str(row['unit']).lower() == 'м2':
+                    geogrids_nt += float(row['amount'])
+            """расчет теплоизоляционного материала"""
+            if (str(row['code']).lower().find('фссц-12.2.04') == 0 or str(row['code']).lower().find('12.2.04') == 0) and (str(row['name']).lower().find('маты') == 0 or str(row['name']).lower().find('пакеты') == 0):
+                if str(row['unit']).lower() == 'м3':
+                    thermal += float(row['amount'])
+            """расчет битумной мастики"""
+            if (str(row['code']).lower().find('01.2.03.03') == 0 or str(row['code']).lower().find('фссц-01.2.03.03') == 0) and (str(row['name']).lower().find('мастика ') == 0 or str(row['name']).lower().find('состав мастичный') == 0):
+                if str(row['unit']).lower() == 'кг':
+                    bituminous += float(row['amount'])
+                elif str(row['unit']).lower() == 'т':
+                    bituminous += float(row['amount']) * 1000
+            """расчет ЛКМ"""
+            #расчет грунтовки группа 14.4.01
+            if (str(row['code']).lower().find('14.4.01.') == 0 or str(row['code']).lower().find('фссц-14.4.01.') == 0) and (str(row['name']).lower().find('грунтовка') == 0 or str(row['name']).lower().find('праймер') == 0 or str(row['name']).lower().find('cостав') == 0 or str(row['name']).lower().find('грунт') == 0 or str(row['name']).lower().find('покрытие') == 0):
+                if str(row['unit']).lower() == 'кг':
+                    paints += float(row['amount'])
+                elif str(row['unit']).lower() == 'т':
+                    paints += float(row['amount']) * 1000
+            #расчет ЛКМ группы 14.4.02
+            if (str(row['code']).lower().find('14.4.02.') == 0 or str(row['code']).lower().find('фссц-14.4.02.') == 0) and (str(row['name']).lower().find('белила') == 0 or str(row['name']).lower().find('краска') == 0 or str(row['name']).lower().find('покрытие') == 0 or str(row['name']).lower().find('эмаль') == 0 or str(row['name']).lower().find('композиция') == 0 or str(row['name']).lower().find('топкоут') == 0):
+                if str(row['unit']).lower() == 'кг':
+                    paints += float(row['amount'])
+                elif str(row['unit']).lower() == 'т':
+                    paints += float(row['amount']) * 1000
+            #расчет лаки группы 14.4.03
+            if (str(row['code']).lower().find('14.4.03.') == 0 or str(row['code']).lower().find('фссц-14.4.03.') == 0) and (str(row['name']).lower().find('лак') == 0 or str(row['name']).lower().find('раствор хлорсульфированного') == 0 or str(row['name']).lower().find('нитролак') == 0 or str(row['name']).lower().find('покрытие полиуретановое КТ ') == 0):
+                if str(row['unit']).lower() == 'кг':
+                    paints += float(row['amount'])
+                elif str(row['unit']).lower() == 'т':
+                    paints += float(row['amount']) * 1000
+            #расчет эмали группы 14.4.04
+            if (str(row['code']).lower().find('14.4.04.') == 0 or str(row['code']).lower().find('фссц-14.4.04.') == 0) and (str(row['name']).lower().find('нитроэмаль') == 0 or str(row['name']).lower().find('эмаль') == 0 or str(row['name']).lower().find('покрытие') == 0 or str(row['name']).lower().find('состав (эмаль)') == 0 or str(row['name']).lower().find('шликер') == 0 or str(row['name']).lower().find('спрей-эмаль') == 0 or str(row['name']).lower().find('эпималь') == 0):
+                if str(row['unit']).lower() == 'кг':
+                    paints += float(row['amount'])
+                elif str(row['unit']).lower() == 'т':
+                    paints += float(row['amount']) * 1000
+            #другое Прайс-лист 2.1
+            if str(row['code']).lower().find('прайс-лист') == 0 and (str(row['name']).lower().find('эмаль') != -1):
+                if str(row['unit']).lower() == 'кг':
+                    paints += float(row['amount'])
+                elif str(row['unit']).lower() == 'т':
+                    paints += float(row['amount']) * 1000
+
         global materials
         materials = [['Наименование основных строительных конструкций, изделий и материалов', 'Единица измерения', 'Всего']]
-        electrodes = ['Электроды', 'кг', round(electrodes*1000, 3)]
-        materials.append(electrodes)
+        electrodes = ['Электроды', 'кг', round(electrodes*1000, 2)]
+        paints = ['Лакокрасочные материалы', 'кг', round(paints, 2)]
+        geosynthetic_nt = ['Нетканое геополотно', 'м2', round(geosynthetic_nt, 2)]
+        geogrids_nt = ['Нетканая георешетка', 'м2', round(geogrids_nt, 2)]
+        thermal = ['Теплоизоляционный материал', 'м3', round(thermal, 2)]
+        bituminous = ['Битумно-резиновая мастика', 'кг', round(bituminous, 2)]
+
+        if electrodes[2] != 0:
+            materials.append(electrodes)
+        if paints[2] != 0:
+            materials.append(paints)
+        if geosynthetic_nt[2] != 0:
+            materials.append(geosynthetic_nt)
+        if geogrids_nt[2] != 0:
+            materials.append(geogrids_nt)
+        if thermal[2] != 0:
+            materials.append(thermal)
+        if bituminous[2] != 0:
+            materials.append(bituminous)
 
     def showDialog(self):
         try:
@@ -63,57 +136,62 @@ class resourses_sheet(QMainWindow):
         name_pd = self.UI.name_pd.toPlainText()
         chifr = self.UI.chifr.toPlainText()
         fn = 'Задание отделу ЭиПБ' #название сохраняемого файла
-
-        doc = docx.Document()
-        style = doc.styles['Normal']
-        font = style.font
-        font.name = 'Times New Romans'
-        font.size = Pt(12)
-        a = doc.add_paragraph()
-        a.add_run('ЗАДАНИЕ ОТДЕЛУ ЭиПБ').bold = True
-        a.alignment = 1
-        b = doc.add_paragraph(f'Заказ № {chifr}\nСтадия ПД')
-        b.alignment = 2
-        doc.add_paragraph('')
-        table = doc.add_table(rows=3, cols=2, style='Table Grid')
-        table.cell(0, 0).text = 'От отдела\n'
-        table.cell(0, 1).text = 'СМ'
-        table.cell(1, 0).text = 'Отделу\n'
-        table.cell(1, 1).text = 'ЭиПБ'
-        table.cell(2, 0).text = 'Наименование объекта\n'
-        table.cell(2, 1).text = f'{name_pd}'
-        table = doc.add_table(rows=1, cols=1, style='Table Grid')
-        table.cell(0, 0).text = 'Ведомость материалов для проведения расчетов и оценки негативного воздействия'
-        doc.add_paragraph()
-        table = doc.add_table(rows=len(materials), cols=3, style='Table Grid')
-        table.autofit = False
-        table.allow_autofit = False
-        table.columns[0].width = Inches(1.5)
-        table.rows[0].cells[0].width = Inches(3.0)
-        table.columns[0].width = Inches(1.5)
-        table.rows[0].cells[1].width = Inches(1)
-        x = 0
-        y = 0
-        for row in table.rows:
-            for cell in row.cells:
-                cell.text = str(materials[x][y])
-                y += 1
-            x += 1
+        try:
+            doc = docx.Document()
+            style = doc.styles['Normal']
+            font = style.font
+            font.name = 'Times New Romans'
+            font.size = Pt(12)
+            a = doc.add_paragraph()
+            a.add_run('ЗАДАНИЕ ОТДЕЛУ ЭиПБ').bold = True
+            a.alignment = 1
+            b = doc.add_paragraph(f'Заказ № {chifr}\nСтадия ПД')
+            b.alignment = 2
+            doc.add_paragraph('')
+            table = doc.add_table(rows=3, cols=2, style='Table Grid')
+            table.cell(0, 0).text = 'От отдела\n'
+            table.cell(0, 1).text = 'СМ'
+            table.cell(1, 0).text = 'Отделу\n'
+            table.cell(1, 1).text = 'ЭиПБ'
+            table.cell(2, 0).text = 'Наименование объекта\n'
+            table.cell(2, 1).text = f'{name_pd}'
+            table = doc.add_table(rows=1, cols=1, style='Table Grid')
+            table.cell(0, 0).text = 'Ведомость материалов для проведения расчетов и оценки негативного воздействия'
+            doc.add_paragraph()
+            table = doc.add_table(rows=len(materials), cols=3, style='Table Grid')
+            table.autofit = False
+            table.allow_autofit = False
+            table.columns[0].width = Inches(1.0)
+            table.rows[0].cells[0].width = Inches(1.0)
+            table.columns[0].width = Inches(1.0)
+            table.rows[0].cells[1].width = Inches(1.0)
+            x = 0
             y = 0
-        for row in table.rows:
-            for cell in row.cells:
-                paragraphs = cell.paragraphs
-                paragraph = paragraphs[0]
-                run_obj = paragraph.runs
-                run = run_obj[0]
-                font = run.font
-                font.name = 'Arial'
-                font.size = Pt(10)
-        saves = QFileDialog.getSaveFileName(self,
-            self.tr("Save file"), fn,
-            self.tr("Doc files (*.docx *.doc)"))[0]
-        if saves:
-            doc.save(saves)
+            for row in table.rows:
+                for cell in row.cells:
+                    cell.text = str(materials[x][y])
+                    y += 1
+                x += 1
+                y = 0
+            for row in table.rows:
+                for cell in row.cells:
+                    paragraphs = cell.paragraphs
+                    paragraph = paragraphs[0]
+                    run_obj = paragraph.runs
+                    run = run_obj[0]
+                    font = run.font
+                    font.name = 'Arial'
+                    font.size = Pt(10)
+            saves = QFileDialog.getSaveFileName(self,
+                                                self.tr("Save file"), fn,
+                                                self.tr("Doc files (*.docx *.doc)"))[0]
+            if saves:
+                try:
+                    doc.save(saves)
+                except PermissionError:
+                    QMessageBox.critical(self, "Ошибка ", "Файл не может быть сохранен, так как открыт в другой программе.", QMessageBox.Ok)
+        except NameError:
+            QMessageBox.critical(self, "Ошибка ", "Не выбран файл", QMessageBox.Ok)
 
     def init_UI(self):
         self.setWindowTitle("Анализ ресурсной ведомости")
@@ -127,3 +205,4 @@ application = resourses_sheet()
 application.show()
 
 sys.exit(app.exec_())
+input('Press ENTER to exit')
